43a0b78e3c2c8559a1143624901c46dcd03bf478
Ilias Gerostathopoulos <iliasger@gmail.com>
2024-08-02 19:40:50 +0300

    first working version of the integration between portal, exp_engine, and proactive!

--- exp_engine/api.py
+++ exp_engine/api.py
@@ -2,7 +2,9 @@ from flask import Flask, request
 from flask_cors import CORS, cross_origin
 from apiHandler import apiHandler
 import logging
-logging.basicConfig(level=logging.DEBUG)
+logging.basicConfig(level=logging.INFO)
+from translation import json2dsl
+import pprint
 
 app = Flask(__name__)
 cors = CORS(app) # cors is added in advance to allow cors requests
@@ -14,7 +16,15 @@ app.config['CORS_HEADERS'] = 'Content-Type'
 def run():
     if request.method == 'POST':
         posted_data = request.get_json() # get_data gets the body of post request
-        app.logger.info('testing info log')
-        app.logger.info(posted_data)
+        app.logger.info('Received request to run experiment: ')
+        json_data = posted_data['graphical_model']
+        dsl_output = json2dsl.json_to_dsl(json_data)
+        app.logger.info(dsl_output)
+
+        with open("workflows/IDEKO_main.xxp", "w") as file: file.write(dsl_output)
+        nodes = {node['id']: node for node in json_data['nodes']}
+        dsl_lines = json2dsl.extract_and_save_composite_node_details(nodes)
+        with open("workflows/IDEKO_DataPreprocessing.xxp", 'w') as file: file.write(dsl_lines)
+
         apiHandler.run_experiment()
         return {"message": "experiment started"}, 201
--- exp_engine/apiHandler.py
+++ exp_engine/apiHandler.py
@@ -1,21 +1,23 @@
 import exp_engine_functions as functions
+import logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 
 class ApiHandler(object):
 
-    EXPERIMENTS_FOLDER = 'IDEKO-experiment1/'
+    EXPERIMENTS_FOLDER = 'workflows/'
 
     def __init__(self):
         print("ApiHandler created")
 
     def run_experiment(self):
-        print("run_experiment")
-
         dsl_file="IDEKO_main"
 
         with open(self.EXPERIMENTS_FOLDER + dsl_file + '.xxp', 'r') as file:
             experiment_specification = file.read()
 
+        logger.info("running experiment NOW")
         functions.run_experiment(experiment_specification)
 
 
