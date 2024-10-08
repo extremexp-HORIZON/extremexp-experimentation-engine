2e1899ff1a8e37fac97b25c0bcb6835d44af2ed9
Ilias Gerostathopoulos <iliasger@gmail.com>
2024-10-08 14:09:43 +0200

    new runner working with data abstraction layer

--- exp_engine/run_experiment.py
+++ exp_engine/run_experiment.py
@@ -1,17 +1,18 @@
 import exp_engine_functions as functions
+from data_abstraction_layer.data_abstraction_api import create_experiment
 
 EXPERIMENTS_FOLDER = 'IDEKO-experiment1/'
-
-# dsl_file = input("Please provide the name of the DSL file (without the extension):")
+# EXPERIMENTS_FOLDER = 'MOBY-experiment1/'
 
 dsl_file="IDEKO_high"
+# dsl_file="moby-exp1"
 
 with open(EXPERIMENTS_FOLDER + dsl_file + '.xxp', 'r') as file:
     workflow_specification = file.read()
 
-experiment_specifications = functions.get_experiment_specification(workflow_specification)
-
-for ep in experiment_specifications:
-    print(ep)
-    # functions.run_experiment(ep)
-
+new_exp = {
+    'name': "TestExperiment",
+    'model': str(workflow_specification),
+}
+exp_id = create_experiment(new_exp)
+functions.run_experiment(workflow_specification, exp_id)
