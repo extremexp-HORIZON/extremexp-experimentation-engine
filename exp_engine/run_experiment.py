3e02e7a22d2028543d67df3ef4cd223b8a11a6f1
Keerthiga <k.rajenthiram@vu.nl>
2024-10-14 14:57:55 +0200

    updating IDEKO_main.xxp with an additional space

--- exp_engine/run_experiment.py
+++ exp_engine/run_experiment.py
@@ -4,15 +4,25 @@ from data_abstraction_layer.data_abstraction_api import create_experiment
 EXPERIMENTS_FOLDER = 'IDEKO-experiment1/'
 # EXPERIMENTS_FOLDER = 'MOBY-experiment1/'
 
-dsl_file="IDEKO_high"
-# dsl_file="moby-exp1"
+# dsl_file="IDEKO_high"
+dsl_file="moby-exp1"
+dsl_file="IDEKO_binary_main"
 
 with open(EXPERIMENTS_FOLDER + dsl_file + '.xxp', 'r') as file:
     workflow_specification = file.read()
 
 new_exp = {
-    'name': "TestExperiment",
+    'name': "IDEKO_Experiment",
     'model': str(workflow_specification),
 }
+
 exp_id = create_experiment(new_exp)
+
 functions.run_experiment(workflow_specification, exp_id)
+
+# experiment_specifications = functions.get_experiment_specification(workflow_specification)
+#
+# for ep in experiment_specifications:
+#     print(ep)
+#     functions.run_experiment(ep)
+
