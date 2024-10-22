d1095b57381b5121d63798db2f502383e1c6a9e0
Ilias Gerostathopoulos <iliasger@gmail.com>
2024-10-22 15:13:06 +0200

    adding example script that uses eexp_engine module and template of config file to be used together with it, git ignoring the actual config file

---
+++ eexp_config_TEMPLATE.py
@@ -0,0 +1,14 @@
+# Main folders (mandatory) - paths are just examples
+TASK_LIBRARY_PATH = 'exp_engine/library-tasks'
+EXPERIMENT_LIBRARY_PATH = 'exp_engine/library-experiments'
+# The ones below has to be a path relative to the client script
+DATASET_LIBRARY_RELATIVE_PATH = 'exp_engine/library-datasets'
+PYTHON_DEPENDENCIES_RELATIVE_PATH = 'exp_engine/tasks'
+
+# Proactive credentials (mandatory - ask ACTIVEEON)
+PROACTIVE_USERNAME=""
+PROACTIVE_PASSWORD=""
+
+# Data abstraction credentials (mandatory - ask CUNI)
+DATA_ABSTRACTION_BASE_URL = "https://api.expvis.smartarch.cz/api"
+DATA_ABSTRACTION_ACCESS_TOKEN = ''
---
+++ example.py
@@ -0,0 +1,5 @@
+from eexp_engine import runner
+import eexp_config
+
+exp_name = 'IDEKO_main'
+runner.run(__file__, exp_name, eexp_config)
