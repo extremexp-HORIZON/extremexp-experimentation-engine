101253ea3d6ab183a14a9315038f5a4dfefde745
Ilias Gerostathopoulos <iliasger@gmail.com>
2024-11-19 19:21:55 +0100

    refactoring #1: splitting classes into several files

--- exp_engine/src/eexp_engine/classes/task.py
+++ exp_engine/src/eexp_engine/classes/task.py
@@ -1,30 +1,4 @@
-import os
-
-class WorkflowDataset:
-
-    def __init__(self, name):
-        self.name = name
-        self.name_in_task_signature = None
-        self.name_in_generating_task = None
-        self.path = ""
-
-    def add_path(self, path):
-        self.path = path
-
-    def set_name_in_task_signature(self, name_in_task_signature):
-        self.name_in_task_signature = name_in_task_signature
-
-    def set_name_in_generating_task(self, name_in_generating_task):
-        self.name_in_generating_task = name_in_generating_task
-
-    def print(self, tab=""):
-        print(f"{tab}\twith dataset name : {self.name}")
-        print(f"{tab}\twith dataset name in task signature : {self.name_in_task_signature}")
-        print(f"{tab}\twith dataset name in generating task : {self.name_in_generating_task}")
-        print(f"{tab}\twith dataset path : {self.path}")
-
-
-class WorkflowTask:
+class Task:
 
     def __init__(self, name):
         self.name = name
@@ -77,6 +51,7 @@ class WorkflowTask:
         self.sub_workflow = workflow
 
     def add_dependent_module(self, folder, name):
+        import os
         dependent_module_path = os.path.join(folder, name)
         self.dependent_modules.append(dependent_module_path)
 
@@ -102,7 +77,7 @@ class WorkflowTask:
         self.prototypical_outputs = prototypical_outputs
 
     def clone(self, parsed_workflows=None):
-        new_t = WorkflowTask(self.name)
+        new_t = Task(self.name)
         new_t.prototypical_name = self.prototypical_name
         new_t.prototypical_inputs = self.prototypical_inputs
         new_t.prototypical_outputs = self.prototypical_outputs
@@ -154,83 +129,3 @@ class WorkflowTask:
         # print(f"{tab}\twith if_task_name: {self.if_task_name}")
         # print(f"{tab}\twith else_task_name: {self.else_task_name}")
         # print(f"{tab}\twith continuation_task_name: {self.continuation_task_name}")
-
-
-class Metric:
-
-    def __init__(self, name, semantic_type, kind, data_type):
-        self.name = name
-        self.semantic_type = semantic_type
-        self.kind = kind
-        self.data_type = data_type
-
-    def print(self, tab=""):
-        print(f"{tab}\twith metric name : {self.name}")
-        print(f"{tab}\twith metric semantic type : {self.semantic_type}")
-        print(f"{tab}\twith metric kind : {self.kind}")
-        print(f"{tab}\twith metric data_type : {self.data_type}")
-
-    def clone(self):
-        new_m = Metric(self.name, self.semantic_type, self.kind, self.data_type)
-        return new_m
-
-
-class Workflow:
-
-    def __init__(self, name):
-        self.is_main = None
-        self.name = name
-        self.tasks = []
-        self.datasets = []
-
-    def add_task(self, task):
-        self.tasks.append(task)
-
-    def add_dataset(self, dataset):
-        self.datasets.append(dataset)
-
-    def get_task(self, name):
-        return next(t for t in self.tasks if t.name == name)
-
-    def get_dataset(self, name):
-        return next(ds for ds in self.datasets if ds.name == name)
-
-    def is_flat(self):
-        for t in self.tasks:
-            if t.sub_workflow:
-                return False
-        return True
-
-    def set_is_main(self, is_main):
-        self.is_main = is_main
-
-    def clone(self, parsed_workflows=None):
-        new_w = Workflow(self.name)
-        new_w.is_main = self.is_main
-        for t in self.tasks:
-            new_t = t.clone(parsed_workflows)
-            new_w.tasks.append(new_t)
-        return new_w
-
-    def print(self, tab=""):
-        print(f"{tab}Workflow with name: {self.name}")
-        print(f"{tab}Workflow is main?: {self.is_main}")
-        print(f"{tab}Workflow is flat?: {self.is_flat()}")
-        for t in sorted(self.tasks, key=lambda t: t.order):
-            t.print(tab+"\t")
-            if t.sub_workflow:
-                t.sub_workflow.print(tab+"\t\t")
-
-
-class AutomatedEvent:
-    def __init__(self, name, task, condition):
-        self.name = name
-        self.task = task
-        self.condition = condition
-
-
-class ManualEvent:
-    def __init__(self, name, task, restart=False):
-        self.name = name
-        self.task = task
-        self.restart = restart
