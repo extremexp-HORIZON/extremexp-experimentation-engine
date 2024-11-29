02c2a0df2ec2640cbef8c8047a0ad8237071a375
Ilias Gerostathopoulos <iliasger@gmail.com>
2024-11-29 14:39:19 +0100

    returning a single element if the list contains only this one in load_datasets helpers

--- exp_engine/src/eexp_engine/proactive_executionware/proactive_helper.py
+++ exp_engine/src/eexp_engine/proactive_executionware/proactive_helper.py
@@ -16,7 +16,10 @@ def save_datasets(variables, *data):
 
 
 def load_datasets(variables, *keys):
-    return [_load_dataset(variables, key) for key in keys]
+    datasets = [_load_dataset(variables, key) for key in keys]
+    if len(datasets) == 1:
+        return datasets[0]
+    return datasets
 
 
 def _save_dataset(variables, key, value):
