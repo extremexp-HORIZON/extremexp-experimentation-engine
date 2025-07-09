66f24052a3173158c8ab4b8f1843990a146fc22f
Ilias Gerostathopoulos <iliasger@gmail.com>
2025-07-09 10:45:31 +0200

    renaming example experiments to to remove zenoh part in their name

--- example.py
+++ example.py
@@ -7,7 +7,8 @@ import eexp_config
 # exp_name = 'moby-exp1'
 # exp_name = 'user_interaction_in_experiment'
 # exp_name = 'user_interaction_in_workflow'
-exp_name = 'test_zenoh'
+exp_name = 'test_ddm'
+# exp_name = 'test_ddm_folders'
 # exp_name = 'test_local'
 
 client.run(__file__, exp_name, eexp_config)
