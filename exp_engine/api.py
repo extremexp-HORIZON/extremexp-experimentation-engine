7c38127bc8ea720c200628b966e5bd096e3106e3
Ilias Gerostathopoulos <iliasger@gmail.com>
2025-07-09 09:59:25 +0200

    changing http codes in api

--- exp_engine/api.py
+++ exp_engine/api.py
@@ -20,7 +20,7 @@ def run(experimentname):
     try:
         print("Running new experiment")
         exp_id = apiHandler.run_exp(experimentname)
-        return {"message": f"experiment finished with id {exp_id}"}, 201
+        return {"message": f"experiment finished with id {exp_id}"}, 200
     except Exception as e:
         print(f"Exception: {e}", flush=True)
         return {"message": f"exception: {e}"}, 500
@@ -63,37 +63,37 @@ def run(experimentname):
 @cross_origin()
 def kill_workflow(workflow_id):
     apiHandler.kill_workflow(workflow_id)
-    return {"message": f"workflow with id {workflow_id} is killed"}, 201
+    return {"message": f"workflow with id {workflow_id} is killed"}, 204
 
 
 @app.route("/exp/workflow/pause/<workflow_id>", methods=["GET"])
 @cross_origin()
 def pause_workflow(workflow_id):
     apiHandler.pause_workflow(workflow_id)
-    return {"message": f"workflow with id {workflow_id} is paused"}, 201
+    return {"message": f"workflow with id {workflow_id} is paused"}, 204
 
 
 @app.route("/exp/workflow/resume/<workflow_id>", methods=["GET"])
 @cross_origin()
 def resume_workflow(workflow_id):
     apiHandler.resume_workflow(workflow_id)
-    return {"message": f"workflow with id {workflow_id} is resumed"}, 201
+    return {"message": f"workflow with id {workflow_id} is resumed"}, 204
 
 
 @app.route("/exp/experiment/kill/<experiment_id>", methods=["GET"])
 @cross_origin()
 def kill_experiment(experiment_id):
     apiHandler.kill_experiment(experiment_id)
-    return {"message": f"experiment with id {experiment_id} is killed"}, 201
+    return {"message": f"experiment with id {experiment_id} is killed"}, 204
 
 @app.route("/exp/experiment/pause/<experiment_id>", methods=["GET"])
 @cross_origin()
 def pause_experiment(experiment_id):
     apiHandler.pause_experiment(experiment_id)
-    return {"message": f"experiment with id {experiment_id} is paused"}, 201
+    return {"message": f"experiment with id {experiment_id} is paused"}, 204
 
 @app.route("/exp/experiment/resume/<experiment_id>", methods=["GET"])
 @cross_origin()
 def resume_experiment(experiment_id):
     apiHandler.resume_experiment(experiment_id)
-    return {"message": f"experiment with id {experiment_id} is resumed"}, 201
+    return {"message": f"experiment with id {experiment_id} is resumed"}, 204
