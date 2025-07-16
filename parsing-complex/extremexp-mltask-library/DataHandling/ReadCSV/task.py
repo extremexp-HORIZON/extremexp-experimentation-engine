3ef03b2ab857dbf084f45e901b06208ba06d89cf
Tsosko <kon.kozanis@gmail.com>
2025-07-16 13:01:31 +0200

    Repository cleanup & grouping of test experiments

--- parsing-complex/extremexp-mltask-library/DataHandling/ReadCSV/task.py
+++
@@ -1,8 +0,0 @@
-import proactive_helper as ph
-import pandas as pd 
-
-input_file_path = ph.get_input("ExternalDataFile")
-
-df = pd.read_csv(zip_file.open(input_file_path), delimiter = ";")
-
-ph.save_output("Dataframe", df)
--- streamlit/View.py
+++
@@ -1,4 +0,0 @@
-import streamlit as st
-
-def display_new_page():
-    st.write("hello")
