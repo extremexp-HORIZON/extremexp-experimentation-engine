23cd8999a9b35eb9627d3d27b94219c0b549298b
kkozanis <kon.kozanis@gmail.com>
2025-07-30 12:12:13 +0200

    Refactor code structure and functionalities for local execution

--- exp_engine/library-tasks/demo_tasks/DemoWP5Task2V1/task.py
+++ exp_engine/library-tasks/demo_tasks/DemoWP5Task2V1/task.py
@@ -5,4 +5,4 @@ print("Running DemoWP5Task2V1")
 
 dataset = ph.load_dataset(variables, resultMap, "DemoWP5Task2InputFile")
 
-ph.save_dataset(variables, resultMap, "DemoWP5Task2OutputFile", dataset)
+ph.save_dataset(variables, resultMap, "DemoWP5Task2OutputFile", dataset.to_csv(index=False))
