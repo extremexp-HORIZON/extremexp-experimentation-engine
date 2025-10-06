c1d751875630ee06a96ac939c17ac16302e69c3a
kkozanis <kon.kozanis@gmail.com>
2025-10-06 11:37:38 +0200

    Update experiment configurations and enhance error handling in execution workflow
    - Change input and output file formats in test_ddm.xxp from JSON to CSV.
    - Modify control flow in user_interaction_in_experiment.xxp to include an additional step.
    - Improve error logging in proactive_runner.py during gateway teardown and file removal.
    - Adjust execution.py to handle results serialization and improve error handling in subprocesses.

--- example.py
+++ example.py
@@ -7,8 +7,9 @@ import eexp_config
 # exp_name = 'moby-exp1'
 # exp_name = 'user_interaction_in_experiment'
 # exp_name = 'user_interaction_in_workflow'
-exp_name = 'test_ddm'
+exp_name = 'my_test'
 # exp_name = 'test_ddm_folders'
 # exp_name = 'test_local'
 
-client.run(__file__, exp_name, eexp_config)
+if __name__ == "__main__":
+    client.run(__file__, exp_name, eexp_config)
